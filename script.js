bab3920ef075c82b49d42102cf057652307ad1b3
Brian Trickovic <me@briantrickovic.com>
2023-02-03 11:55:47 -0500

    add States API and Handlebars in script.js + add statesTemp in HTML

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const countriesUrl = 'https://xc-countries-api.fly.dev/api/countries/';
+let statesUrl;
+let currentId;
+let currentCode;
+
 
 // Countries API and Handlebars
 fetch(countriesUrl)
@@ -6,6 +10,7 @@ fetch(countriesUrl)
   .then(data => {
     let apiData = data;
     
+    // Handlebars
     const source = document.getElementById('countriesTemp').innerHTML;
     const template = Handlebars.compile(source);
 
@@ -16,6 +21,45 @@ fetch(countriesUrl)
 
     const displayCountries = document.getElementById('countries');
     displayCountries.innerHTML = compiledHtml;
+
+    // Gets currently selected value from user on Countries dropdown menu
+    displayCountries.addEventListener("change", function() {
+      // Get the selected value
+      let selectedValue = this.value;
+    
+      if (selectedValue === 'Australia') {
+        currentId = apiData[0]["id"];
+        currentCode = apiData[0]["code"];
+      } else if (selectedValue === 'Canada') {
+        currentId = apiData[1]["id"];
+        currentCode = apiData[1]["code"];
+      } else {
+        currentId = apiData[2]["id"];
+        currentCode = apiData[2]["code"];
+      }
+
+      statesUrl = `https://xc-countries-api.fly.dev/api/countries/${currentCode}/states/`;
+      
+      // States API and Handlebars
+      fetch(statesUrl)
+        .then(response => response.json())
+        .then(data => {
+          let apiData = data;
+
+          // Handlebars
+          const source = document.getElementById('statesTemp').innerHTML;
+          const template = Handlebars.compile(source);
+
+          const context = {
+            state: apiData
+          }
+          const compiledHtml = template(context);
+
+          const displayStates = document.getElementById('states');
+          displayStates.innerHTML = compiledHtml;
+        })
+        .catch(error => console.error(error));
+    });
   })
   .catch(error => console.error(error));
 
