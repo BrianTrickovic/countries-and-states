62e3c4993b7e5498c870886928def5758fa53b2c
Brian Trickovic <me@briantrickovic.com>
2023-02-06 10:32:40 -0500

    add alphabetical sorter by name for API data

--- script.js
+++ script.js
@@ -14,6 +14,17 @@ fetch(countriesUrl)
     const source = document.getElementById('countriesTemp').innerHTML;
     const template = Handlebars.compile(source);
 
+    // Sorts Countries API data alphabetically by name
+    apiData.sort(function (a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+
     const context = {
       country: apiData
     }
