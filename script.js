92d7dcfcba44e336884db1ad00081b64c0a148b6
Brian Trickovic <me@briantrickovic.com>
2023-02-06 16:32:01 -0500

    change input text and label order in index.html, add API data sorter by id value and get lowest id value available to attach to a new country object in script.js

--- script.js
+++ script.js
@@ -12,10 +12,22 @@ function sortData (apiData) {
   });
 }
 
+function compareIdNumbers (a, b) {
+  return a.id - b.id;
+}
+
 function findIndex (apiData, selectedValue) {
   for (let i = 0; i < apiData.length; i++) {
     if (selectedValue === apiData[i].name) {
       return i;
+    }
+  }
+}
+
+function getNextId (apiData) {
+  for (let i = 0; i < apiData.length; i++) {
+    if (apiData[i].id != (apiData[i + 1].id - 1)) {
+      return apiData[i].id + 1;
     }
   }
 }
@@ -82,49 +94,63 @@ fetch(countriesUrl)
         })
         .catch(error => console.error(error));
     });
+
+    // POST form section
+
+    apiData.sort(compareIdNumbers);
+    // calculates the lowest available id value
+    let nextId;
+    try {
+      nextId = getNextId(apiData);
+    } catch (error) {
+      nextId = apiData.length + 1;
+    }
+
+    // New Country form
+    const form2 = document.getElementById("form2");
+    form2.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const value1 = nextId;
+      const value2 = document.getElementById("code").value;
+      const value3 = document.getElementById("name").value;
+      const response = await fetch("https://xc-countries-api.fly.dev/api/countries/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id: value1, code: value2, name: value3 }),
+      });
+      if (response.ok) {
+        const selectElement = document.getElementById("countries");
+        const optionElement = document.createElement("option");
+        optionElement.value = value3;
+        optionElement.textContent = value3;
+        selectElement.appendChild(optionElement);
+      }
+    });
+
+    // New State form
+    const form3 = document.getElementById("form3");
+    form3.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const value1 = document.getElementById("code2").value;
+      const value2 = document.getElementById("name2").value;
+      console.log(value1);
+      const response = await fetch("https://xc-countries-api.fly.dev/api/states/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ code: value1, name: value2 }),
+      });
+      if (response.ok) {
+        const selectElement = document.getElementById("states");
+        const optionElement = document.createElement("option");
+        optionElement.value = value2;
+        optionElement.textContent = value2;
+        selectElement.appendChild(optionElement);
+      }
+    });
   })
   .catch(error => console.error(error));
 
 
-  const form = document.getElementById("form2");
-  form.addEventListener("submit", async (event) => {
-    event.preventDefault();
-    const value1 = document.getElementById("id").value;
-    const value2 = document.getElementById("code").value;
-    const value3 = document.getElementById("name").value;
-    console.log(value1);
-    const response = await fetch("https://xc-countries-api.fly.dev/api/countries/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ id: value1, code: value2, name: value3 }),
-    });
-    if (response.ok) {
-      const selectElement = document.getElementById("countries");
-      const optionElement = document.createElement("option");
-      optionElement.value = value3;
-      optionElement.textContent = value3;
-      selectElement.appendChild(optionElement);
-    }
-  });
-
-  const form3 = document.getElementById("form3");
-  form3.addEventListener("submit", async (event) => {
-    event.preventDefault();
-    const value1 = document.getElementById("code2").value;
-    const value2 = document.getElementById("name2").value;
-    console.log(value1);
-    const response = await fetch("https://xc-countries-api.fly.dev/api/states/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ code: value1, name: value2 }),
-    });
-    if (response.ok) {
-      const selectElement = document.getElementById("states");
-      const optionElement = document.createElement("option");
-      optionElement.value = value2;
-      optionElement.textContent = value2;
-      selectElement.appendChild(optionElement);
-    }
-  });
+  
 
 
